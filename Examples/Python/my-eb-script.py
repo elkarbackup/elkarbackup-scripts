25e424d9b77ed378f79f62a8d837bf7bcf0be5a3
Xabi Ezpeleta <xezpeleta@gmail.com>
2015-03-23 23:05:57 +0100

    Added new environments variables

--- Examples/Python/my-eb-script.py
+++ Examples/Python/my-eb-script.py
@@ -6,13 +6,20 @@ FILE = '/tmp/my-eb-script.out'
 
 def getEnvVars():
   env = {
-    'level': str(os.environ['ELKARBACKUP_LEVEL']),
-    'event': str(os.environ['ELKARBACKUP_EVENT']),
-    'url': str(os.environ['ELKARBACKUP_URL']),
-    'id': str(os.environ['ELKARBACKUP_ID']),
-    'path': str(os.environ['ELKARBACKUP_PATH']),
-    'status': str(os.environ['ELKARBACKUP_STATUS']),
-    'user': str(os.environ['USER'])
+    'level': str(os.environ.get('ELKARBACKUP_LEVEL')),
+    'event': str(os.environ.get('ELKARBACKUP_EVENT')),
+    'url': str(os.environ.get('ELKARBACKUP_URL')),
+    'id': str(os.environ.get('ELKARBACKUP_ID')),
+    'path': str(os.environ.get('ELKARBACKUP_PATH')),
+    'status': str(os.environ.get('ELKARBACKUP_STATUS')),
+    'client-name': str(os.environ.get('ELKARBACKUP_CLIENT_NAME')),
+    'job-name': str(os.environ.get('ELKARBACKUP_JOB_NAME')),
+    'owner-email': str(os.environ.get('ELKARBACKUP_OWNER_EMAIL')),
+    'recipient-list': str(os.environ.get('ELKARBACKUP_RECIPIENT_LIST')),
+    'client-total-size': str(os.environ.get('ELKARBACKUP_CLIENT_TOTAL_SIZE')),
+    'job-total-size': str(os.environ.get('ELKARBACKUP_JOB_TOTAL_SIZE')),
+    'job-run-size': str(os.environ.get('ELKARBACKUP_JOB_RUN_SIZE')),
+    'user': str(os.environ.get('USER'))
   }
   return env
 
