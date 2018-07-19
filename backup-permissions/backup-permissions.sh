d164b6f0b6c48e524fac50a73898398978a6e7a9
Xabi Ezpeleta <xezpeleta@gmail.com>
2018-07-19 12:38:51 +0200

    Embed the ssh functions in the same script
    All-in-one file script, to make easier the script installation

--- backup-permissions/backup-permissions.sh
+++ backup-permissions/backup-permissions.sh
@@ -1,8 +1,31 @@
 #! /bin/bash
 set -e
 
-#source "$(dirname "$0")/lib/eb-debug.sh"
-source "$(dirname "$0")/lib/eb-remote.sh"
+#############################################################################
+## SSH functions
+#############################################################################
+
+#ELKARBACKUP_URL = user@serverip:/path
+URL=`echo $ELKARBACKUP_URL | cut -d ":" -f1`    # user@serverip
+USER="${URL%@*}"                                # user
+HOST="${URL#*@}"                                # host
+SSHPARAMS='-i /var/lib/elkarbackup/.ssh/id_rsa -o StrictHostKeyChecking=no'
+
+# Run a commmand via SSH and return the exit code
+function ssh_exec {
+    cmd="$1"
+    output="$(ssh $SSHPARAMS $USER@$HOST $cmd && echo $?)"
+    return $output
+}
+
+# Run a command via SSH and return the output
+function ssh_exec_output {
+    cmd="$1"
+    output="$(ssh $SSHPARAMS $USER@$HOST $cmd)"
+    echo "$output"
+}
+
+############################################################################
 
 # Requirements
 GETFACL="$(ssh_exec_output "which getfacl")"
